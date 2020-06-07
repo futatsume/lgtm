a959c9dc5d765b056950ff129f3df0b0f4edc344
kikuc <footer2me@gmail.com>
2020-06-07 12:11:19 +0900

    13.4画像処理まで実装

---
+++ lgtm/drawer.py
@@ -0,0 +1,38 @@
+from PIL import Image, ImageDraw, ImageFont
+
+
+# 画像全体に対するメッセージ描画可能エリアの比率
+MAX_RATIO = 0.8
+# フォント関連の定数
+FONT_MAX_SIZE = 256
+FONT_MIN_SIZE = 24
+
+# フォント格納先のパスは実行環境に合わせて変更する
+# FONT_NAME = "C:\Windows\Fonts\Arial\arialbd.ttf"
+FONT_NAME = "arialbd.ttf"
+FONT_COLOR_WHITE = (255, 255, 255, 0)
+
+# アウトプット関連の定数
+OUTPUT_NAME = "output.png"
+OUTPUT_FORMAT = "PNG"
+
+
+def save_with_message(fp, message):
+  image = Image.open(fp)
+  draw = ImageDraw.Draw(image)
+  image_width, image_height = image.size
+  message_area_width = image_width * MAX_RATIO
+  message_area_height = image_height * MAX_RATIO
+  #
+  for font_size in range(FONT_MAX_SIZE, FONT_MIN_SIZE, -1):
+    font = ImageFont.truetype(FONT_NAME, font_size)
+    text_width, text_height = draw.textsize(message, font)
+    w = message_area_width - text_width
+    h = message_area_height - text_height
+
+    if w > 0 and h > 0:
+      position = ((image_width - text_width) / 2,
+                  (image_height - text_height) / 2)
+      draw.text(position, message, fill=FONT_COLOR_WHITE, font=font)
+      break
+  image.save(OUTPUT_NAME, OUTPUT_FORMAT)
